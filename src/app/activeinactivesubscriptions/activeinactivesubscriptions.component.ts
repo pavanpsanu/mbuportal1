c2a3590168550e4b5609cf0f098c1fa9d2e2aab9
pavanpsanu <pavanpsanu@gmail.com>
2021-11-16 11:09:45 +0530

    contact us added

--- src/app/activeinactivesubscriptions/activeinactivesubscriptions.component.ts
+++ src/app/activeinactivesubscriptions/activeinactivesubscriptions.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as XLSX from 'ts-xlsx';
 
 @Component({
   selector: 'app-activeinactivesubscriptions',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./activeinactivesubscriptions.component.css']
 })
 export class ActiveinactivesubscriptionsComponent implements OnInit {
+  
 
   constructor() { }
 
