a1561ac72bbb3194a5089d1416af8042976fc6b4
pavanpsanu <pavanpsanu@gmail.com>
2021-12-03 17:32:04 +0530

    changed the accelerator ip json values

--- src/app/acceleratorsips/acceleratorsips.component.ts
+++ src/app/acceleratorsips/acceleratorsips.component.ts
@@ -9,54 +9,56 @@ export class AcceleratorsipsComponent implements OnInit {
 // bloburl="https://viewdatainexcel.blob.core.windows.net/testcontainer/Accelerator.xlsx"
 
 
-jsonvaluesofexcel= {
+jsonvaluesofexcel={
 	"Sheet1": [{
 			"Slno": 1,
 			"ISU": "CTO",
-			"SubscriptioName": "TCS CUBO Marketplace_API Management",
-			"AcceleratorIp": "TCS-CUBO (IP)",
-			"AccelaratorIPdesc": "Online marketplace for partners to publish their product APIs",
+			"Type": "IP",
+			"AcceleratorIp": "TCS CUBO Marketplace_API Management",
+			"AccelaratorIPdesc": "TCS CUBO Marketplace API Management is a Online marketplace for partners to publish their Product APIs",
+			"OwnerName": "Rajendra Biswal",
+			"OwnerEmailId": "rajendra.biswal @tcs.com ",
 			"Createdon": "07/29/20"
 		},
 		{
 			"Slno": 2,
 			"ISU": "CSI",
-			"SubscriptioName": "DevSecOps PoC",
-			"AcceleratorIp": "DevSec Ops Accelarator",
-			"AccelaratorIPdesc": "Hosting web application, containerized web application in Azure App Service",
+			"Type": "Accelarator",
+			"AcceleratorIp": "DevSecOps Accelarator(Accelarator)",
+			"AccelaratorIPdesc": "Hosting web application,containerized web  application in Azure App Service",
+			"OwnerName": "Deepak Kannan Muthuchamy",
+			"OwnerEmailId": "deepak.kannan @tcs.com ",
 			"Createdon": "02/18/21"
 		},
 		{
 			"Slno": 3,
 			"ISU": "MBU",
-			"SubscriptioName": "mbu_dataai-intelligent_data_services_poc",
-			"AcceleratorIp": "AI Data Service(IP)",
+			"Type": "IP",
+			"AcceleratorIp": "DataAI Intelligent Data Services",
 			"AccelaratorIPdesc": "DataAI intelligent data services",
+			"OwnerName": "Vinit Maind",
+			"OwnerEmailId": "vinit.maind @tcs.com ",
 			"Createdon": "11/24/20"
 		},
 		{
 			"Slno": 4,
 			"ISU": "MBU",
-			"SubscriptioName": "ABN Amro_Smart Document Capturing System",
-			"AcceleratorIp": "DAWIN(IP)",
-			"AccelaratorIPdesc": "Document Automation With Intelligence : - Smart Document Capturing System",
+			"Type": "Accelarator",
+			"AcceleratorIp": "DAWIN - Smart Document Capturing System",
+			"AccelaratorIPdesc": "ABN Amro Smart Document Capturing System is a Document Automation With Intelligence implemented across multiple TCS clients",
+			"OwnerName": "Sanal S",
+			"OwnerEmailId": "sanal.s @tcs.com ",
 			"Createdon": "10/01/20"
 		},
 		{
 			"Slno": 5,
 			"ISU": "CTO",
-			"SubscriptioName": "TCS CUBO Marketplace_API Management",
+			"Type": "Accelarator",
 			"AcceleratorIp": "TCS-CUBO (Accelarator)",
 			"AccelaratorIPdesc": "Online marketplace for partners to publish their Product APIs",
+			"OwnerName": "Rajendra Biswal",
+			"OwnerEmailId": "rajendra.biswal @tcs.com ",
 			"Createdon": "07/29/20"
-		},
-		{
-			"Slno": 6,
-			"ISU": "MBU",
-			"SubscriptioName": "mbu_dataai-intelligent_data_services_poc",
-			"AcceleratorIp": "AI Data Service(Accelarator)",
-			"AccelaratorIPdesc": "DataAI intelligent data services",
-			"Createdon": "11/24/20"
 		}
 	]
 }
